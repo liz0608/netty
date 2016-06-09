1bce1446849eccb033b05d02e629cd299288dba2
liz0608 <liz0608@gmail.com>
2016-06-09 08:49:27 +0800

    添加时间服务器1

---
+++ src/main/java/com/turtle/netty/nio/TimeServer.java
@@ -0,0 +1,44 @@
+package com.turtle.netty.nio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by turtle on 2016/6/9.
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                ;
+            }
+        }
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("The time server is start in port : " + port);
+            Socket socket = null;
+            while (true) {
+                socket = serverSocket.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            ;
+        } finally {
+            if (serverSocket != null) {
+                System.out.println("The time server close");
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                serverSocket = null;
+            }
+        }
+    }
+}
